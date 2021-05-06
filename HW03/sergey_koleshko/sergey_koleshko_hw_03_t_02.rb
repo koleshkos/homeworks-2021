bad70c0b67c945d485190e54dc1486f40fdb972b
Sergey <koleshkoser@gmail.com>
2021-05-06 22:55:39 +0300

    task_2: moving regexp to constants/variables

--- HW03/sergey_koleshko/sergey_koleshko_hw_03_t_02.rb
+++ HW03/sergey_koleshko/sergey_koleshko_hw_03_t_02.rb
@@ -3,9 +3,13 @@ logs = '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTT
 2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
 10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290'
 
+DATA = %r{\d{1,31}/.+/\d{1,2021}:\d+:\d+:\d+ \+\d+}
+FROM = /^\d{0,255}\.\d{0,255}\.\d{0,255}\.\d{0,255}/
+TO = 
+
 def formatted_requests(logs)
   logs.each_line.select { |line| line.match(/POST/) }.map do |l|
-    "#{l.scan(%r{\d{1,31}/.+/\d{1,2021}:\d+:\d+:\d+ \+\d+})[0]} FROM: #{l.scan(/(^\d{0,255}\.\d{0,255}\.\d{0,255}\.\d{0,255})/)[0][0]} TO: #{l.scan(%r{/\w+/\d+/\w+})[0].to_s.upcase}"
+    "#{l.scan(DATA)[0]} FROM: #{l.scan(FROM)[0][0]} TO: #{l.scan(TO)[0].to_s.upcase}"
   end
 end
 
