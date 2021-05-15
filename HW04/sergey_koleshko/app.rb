86ad7f235b425c4632acd426b5f17fb84a046f75
Sergey <koleshkoser@gmail.com>
2021-05-16 01:27:40 +0300

    create metod: notifiications, mark of read, subcribe

--- HW04/sergey_koleshko/app.rb
+++ HW04/sergey_koleshko/app.rb
@@ -1,8 +1,18 @@
-require './mentor.rb'
-require './student.rb'
-require './homework.rb'
+require_relative 'mentor'
+require_relative 'student.rb'
 
-student = Student.new(name:'Joh', surname:'Doe')
-mentor = Mentor.new(name:'Jack', surname:'Gonsales')
+student = Student.new(name: 'John', surname: 'Doe')
+mentor = Mentor.new(name: 'Jack', surname: 'Gonsales')
 
-#homework = mentor.add_homework(title: 'HW03', description: 'description homework', student: student)
+homework = mentor.add_homework(title: 'HW03', description: 'description homework', student: student)
+
+# student check notifications
+student.notifications
+# student mark of read notifications
+student.mark_as_read!
+# notifications is clear
+student.notifications
+# mentor subscribe to student
+mentor.subscribe_to!(student)
+
+student.homeworks
