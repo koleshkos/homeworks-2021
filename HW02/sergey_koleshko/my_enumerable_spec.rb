3f126f067c75ac6ea641e45d831a41276a76db1d
Sergey <koleshkoser@gmail.com>
2021-06-01 00:26:53 +0300

    fixed bugs with name context

--- HW02/sergey_koleshko/my_enumerable_spec.rb
+++ HW02/sergey_koleshko/my_enumerable_spec.rb
@@ -3,96 +3,92 @@ require_relative 'my_enumerable'
 using MyEnumerable
 
 describe MyEnumerable do
-  
   let(:empty_array) { [] }
   let(:array) { [0, 1, 2, 3, 4, 5] }
 
-  # my_each
   context '#my_each' do
-    context "array gets block" do
-      context "when array empty" do
-        it "should return nothing" do 
-          expect(empty_array.my_each {}).to eq ([])
+    context 'when array get block' do
+      context 'when array empty' do
+        it 'should return nothing' do
+          expect(empty_array.my_each {}).to eq([])
         end
       end
-      context "when array empty" do
-        it "should return array" do 
-          expect(array.my_each {}).to eq ([0, 1, 2, 3, 4, 5])
+      context 'when array is empty' do
+        it 'should return array' do
+          expect(array.my_each {}).to eq([0, 1, 2, 3, 4, 5])
         end
       end
     end
-  
-    context "array not gets block" do
-      context "when array empty" do
-        it "should return nothing" do 
-          expect(empty_array.my_each).to eq ([])
+
+    context 'when array not get block' do
+      context 'when array empty' do
+        it 'should return nothing' do
+          expect(empty_array.my_each).to eq([])
         end
       end
-  
-      context "when array not empty" do
-        it "should return nil" do 
-          expect(array.my_each).to eq ([0, 1, 2, 3, 4, 5])
+
+      context 'when array is not empty' do
+        it 'should return nil' do
+          expect(array.my_each).to eq([0, 1, 2, 3, 4, 5])
         end
       end
     end
   end
-  
-  # my_select
+
   context '#my_select' do
-   context 'gets block' do
-     context 'empty array' do
-       it 'should return empty array' do 
-         expect(empty_array.my_select {}).to eq ([])
-       end
-     end
-  
-     context 'not empty array'
-       it 'should return empty array' do 
-         expect(array.my_select {}).to eq ([])
-       end
-     end
-   end
-  
-   context 'not gets block' do
-     context 'empty array' do
-       it 'should return empty array' do 
-         expect(empty_array.my_select).to eq ([])
-       end
-     end
-  
-     context 'not empty array' do
-       it 'should return array' do 
-         expect(array.my_select).to eq ([0, 1, 2, 3, 4, 5])
-       end
-     end
-   end
-
-   #my_map
-   context '#my_map' do
-    context 'gets block' do
-      context 'empty array' do
+    context 'when array get block' do
+      context 'whem array is empty' do
         it 'should return empty array' do
-          expect(empty_array.my_map {}).to eq ([])
+          expect(empty_array.my_select {}).to eq([])
         end
       end
-      context 'not empty array' do
+
+      context 'when array is not empty'
+      it 'should return empty array' do
+        expect(array.my_select {}).to eq([])
+      end
+    end
+  end
+
+  context 'when array not get block' do
+    context 'empty array' do
+      it 'should return empty array' do
+        expect(empty_array.my_select).to eq([])
+      end
+    end
+
+    context 'when array is not empty' do
+      it 'should return array' do
+        expect(array.my_select).to eq([0, 1, 2, 3, 4, 5])
+      end
+    end
+  end
+
+  context '#my_map' do
+    context 'when array get block' do
+      context 'when array is empty' do
+        it 'should return empty array' do
+          expect(empty_array.my_map {}).to eq([])
+        end
+      end
+      context 'when array is not empty' do
         it 'should return array' do
-          expect(array.my_map {}).to eq ([nil, nil, nil, nil, nil, nil])
+          expect(array.my_map {}).to eq([nil, nil, nil, nil, nil, nil])
         end
       end
     end
 
-    context 'not gets block' do
-      context 'empty array' do
+    context 'when array not get block' do
+      context 'when array is empty' do
         it 'should return empty array' do
-          expect(empty_array.my_map).to eq ([])
+          expect(empty_array.my_map).to eq([])
         end
       end
-      context 'not empty array' do
+      context 'when array is not empty' do
         it 'should return array' do
-          expect(array.my_map).to eq ([0, 1, 2, 3, 4, 5])
+          expect(array.my_map).to eq([0, 1, 2, 3, 4, 5])
         end
       end
     end
-   end
+  end
 end
